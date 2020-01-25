e42941398342973608ea78adb612c832fde26e32
negrel <alexnergel13@gmail.com>
2020-01-25 16:06:16 +0100

    improving comments

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,28 @@ use std::mem::drop;
 use std::process::exit;
 
 fn main() {
-	// Use to read user input;
+	// Use to read user input.
 	let mut reader = String::new();
-	// Game variable
+	// Game variable.
 	let mut guess: u32;			// User input (is guess for mystery number).
 	let mut attempt: u32 = 0; 		// Attempt counter.
 	let max_attempt: u32; 	// Max attempt.
 
-	// Banner prompt
+	// Game start prompt.
 	println!("### Guess the number ! ###");
 	
 	// --------------------------------------------------------------------- //
 	// Range of mystery number [min, max[.
 	// --------------------------------------------------------------------- //
-	println!("Enter the mystery number range.");
+	println!("Enter the mystery number range. [min, max[");
 
-	// Min of mystery number
+	// Min of mystery number.
 	println!("Min : ");
 	io::stdin().read_line(&mut reader)
 		.expect("Failed to read line.");
 	let min = parse_to_u32(&mut reader);
 
-	// Max of mystery number
+	// Max of mystery number.
 	println!("Max : ");
 	io::stdin().read_line(&mut reader)
 		.expect("Failed to read line.");
@@ -81,20 +81,20 @@ fn main() {
 	exit(0);
 }
 
-// Return a random number from the given range
+// Return a random number from the given range.
 fn get_random(min: u32, max: u32) -> u32 {
 	return thread_rng().gen_range(min, max);
 }
 
-// Parse the given string to u32
+// Parse the given string to u32.
 fn parse_to_u32(str: &mut String) -> u32 {
-	// Parse the string to u32
+	// Parse the string to u32;
 	let parsed = match str.trim().parse::<u32>() {
 		Ok(num) => num,
 		Err(e) => panic!(e)
 	};
 
-	// Clean the reader
+	// Clean the reader.
 	*str = String::new();
 
 	return parsed;
