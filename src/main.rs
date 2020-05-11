1c9910d0367641b0817b6dcb31bf2dbc46382623
Alexandre Negrel <negrel.dev@protonmail.com>
2020-05-11 22:44:03 +0200

    adding rustfmt config

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ fn main() {
 	// Use to read user input.
 	let mut reader = String::new();
 	// Game variable.
-	let mut guess: u32;			// User input (is guess for mystery number).
-	let mut attempt: u32 = 0; 		// Attempt counter.
-	let max_attempt: u32; 	// Max attempt.
+	let mut guess: u32; // User input (is guess for mystery number).
+	let mut attempt: u32 = 0; // Attempt counter.
+	let max_attempt: u32; // Max attempt.
 
 	// Game start prompt.
 	println!("### Guess the number ! ###");
-	
+
 	// --------------------------------------------------------------------- //
 	// Range of mystery number [min, max[.
 	// --------------------------------------------------------------------- //
@@ -22,20 +22,21 @@ fn main() {
 
 	// Min of mystery number.
 	println!("Min : ");
-	io::stdin().read_line(&mut reader)
+	io::stdin()
+		.read_line(&mut reader)
 		.expect("Failed to read line.");
 	let min = parse_to_u32(&mut reader);
 
 	// Max of mystery number.
 	println!("Max : ");
-	io::stdin().read_line(&mut reader)
+	io::stdin()
+		.read_line(&mut reader)
 		.expect("Failed to read line.");
 	let max = parse_to_u32(&mut reader);
 
-	
 	// Mystery number.
 	let res = get_random(min, max);
-	
+
 	// Clear memory of range variables.
 	drop(min);
 	drop(max);
@@ -43,22 +44,23 @@ fn main() {
 
 	// Number of attempts.
 	println!("How many attempt do you want ?");
-	io::stdin().read_line(&mut reader)
+	io::stdin()
+		.read_line(&mut reader)
 		.expect("Failed to read line.");
-	// Parse to interger.
+	// Parse to integer.
 	max_attempt = parse_to_u32(&mut reader);
-	
+
 	// Ask user guess while attempt counter is lower than max attempt.
 	while attempt < max_attempt {
-
 		// Prompt user to enter a number.
 		println!("Please input your guess.");
 
 		// Read the user input.
-		io::stdin().read_line(&mut reader)
+		io::stdin()
+			.read_line(&mut reader)
 			.expect("Failed to read line.");
 		guess = parse_to_u32(&mut reader);
-		
+
 		// Check the user input.
 		match guess.cmp(&res) {
 			// The user input is smaller than the mystery number.
@@ -92,7 +94,7 @@ fn parse_to_u32(str: &mut String) -> u32 {
 	// Parse the string to u32;
 	let parsed = match str.trim().parse::<u32>() {
 		Ok(num) => num,
-		Err(e) => panic!(e)
+		Err(e) => panic!(e),
 	};
 
 	// Clean the reader.
